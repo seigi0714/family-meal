e7c4beb9a339c1315983d0ba22681fc16d189f5f
seigibus <seigibus@example.com>
2020-05-10 10:01:51 +0900

    firebaseをインストールしました。

---
+++ app/plugins/firebase.js
@@ -0,0 +1,9 @@
+import firebase, { initializeApp } from 'firebase'
+
+if(!firebase.apps.length) {
+    initializeApp({
+        apiKey: "AIzaSyD0BdyLnncHTHUv6QIzPIyrhhPygcR5PlA",
+        authDomain: "family-meal-69f4f.firebaseapp.com",
+        projectId: "family-meal-69f4f"
+    })
+}
